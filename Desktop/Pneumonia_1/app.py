0231bea60078debde76db6b5a4e8cb544dcf7e1f
Vivek <shashank reddy>
2024-10-07 22:39:30 +0530

    updated

--- Desktop/Pneumonia_1/app.py
+++ Desktop/Pneumonia_1/app.py
@@ -2,16 +2,14 @@ from __future__ import division, print_function
 # coding=utf-8
 import sys
 import os
-import glob
-import re
 import numpy as np
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect
 from PIL import Image
 from io import BytesIO
 
 # Keras
 from keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 
 app = Flask(__name__)
 
@@ -22,17 +20,16 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model_path1 = 'Xception.h5'  # Load .h5 Model
-model = load_model(model_path1, compile=False)
+# Load .h5 Model
+model_path = "C:\Users\Shashank\Desktop\Pneumonia_1\xception.h5"
+model = load_model(model_path, compile=False)
 
-def model_predict1(image, model):
-    print("Predicted")
+def model_predict(image, model):
     image = img_to_array(image)
     image = image / 255.0
     image = np.expand_dims(image, axis=0)
-
+    
     result = np.argmax(model.predict(image))
-    print(result)
 
     if result == 0:
         return "NORMAL!", "result.html"
@@ -49,18 +46,14 @@ def predict():
     if request.method == 'POST':
         file = request.files['file']  # Fetch input
         if file and allowed_file(file.filename):
-            print("@@ Input posted = ", file.filename)
-
             # Load image from file without saving to static folder
             image = Image.open(file.stream)
             image = image.resize((128, 128))  # Resize image to the required size
-            pred, output_page = model_predict1(image, model)
+            pred, output_page = model_predict(image, model)
 
             return render_template(output_page, pred_output=pred)
 
     return redirect(request.url)
 
-# Uncomment this to run the app locally
-# if __name__ == '__main__':
-#     app.run(debug=False)
-
+#if __name__ == '__main__':
+    #app.run(debug=True)
